99d46206598d03f80ee726984ba4e7db7388cea3
Arnold Paye <arnoldpayet@gmail.com>
2026-01-12 07:32:26 -0400

    feat: creating a new entity instance in the context

--- src/main/java/com/arnex/app/Main.java
+++ src/main/java/com/arnex/app/Main.java
@@ -1,5 +1,7 @@
 package com.arnex.app;
 
+import com.arnex.app.entities.Book;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -8,5 +10,17 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("library_persistence_unit");
         EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            Book book = new Book();
+            book.setName("my book");
+            book.setIsbn("123-456");
+
+            em.persist(book);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
     }
 }
