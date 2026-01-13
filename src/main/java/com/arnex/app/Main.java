99e96dd4f064d919082cc3a566f3539e75281ee6
Arnold Paye <arnoldpayet@gmail.com>
2026-01-13 05:11:37 -0400

    challenge: art class management app, part 2

--- src/main/java/com/arnex/app/Main.java
+++ src/main/java/com/arnex/app/Main.java
@@ -1,5 +1,7 @@
 package com.arnex.app;
 
+import com.arnex.app.entities.Student;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -8,5 +10,16 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("artclass_persistence_unit");
         EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            Student student = new Student();
+            student.setName("“John”");
+            em.persist(student);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+        
     }
 }
---
+++ src/main/java/com/arnex/app/entities/Review.java
@@ -0,0 +1,48 @@
+package com.arnex.app.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private int id;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @Column(name = "rating")
+    private int rating;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+    
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
