b08db086082a6093bcc81aafa476ee3a0b4dbbc6
Arnold Paye <arnoldpayet@gmail.com>
2026-01-18 11:23:44 -0400

    classart challenge 5

--- src/main/java/com/arnex/app/Main.java
+++ src/main/java/com/arnex/app/Main.java
@@ -11,8 +11,8 @@ public class Main {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("artclass_persistence_unit");
 
         // create(emf);
-        update(emf);
-        attachAndDetach(emf);
+        // update(emf);
+        // attachAndDetach(emf);
         remove(emf);
     }
 
@@ -31,14 +31,51 @@ public class Main {
     }
 
     public static void update(EntityManagerFactory emf) {
+        EntityManager em = emf.createEntityManager();
 
+        try {
+            em.getTransaction().begin();
+
+            Student foundStudent = em.find(Student.class, 4);
+            foundStudent.setName("Peter");
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
     }
 
     public static void attachAndDetach(EntityManagerFactory emf) {
-        
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            Student student = new Student();
+            student.setName("Mary");
+            em.merge(student);
+
+            em.detach(student);
+            student.setName("Arnex");
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
     }
 
     public static void remove(EntityManagerFactory emf) {
-        
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            Student student = em.find(Student.class, 4);
+            em.remove(student);
+            
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
     }
 }
--- src/main/java/com/arnex/app/entities/Student.java
+++ src/main/java/com/arnex/app/entities/Student.java
@@ -33,4 +33,9 @@ public class Student {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + "]";
+    }
 }
