a2a7afbe6bbb3635ac364f2a58a9a96ebd231a5e
Arnold Paye <arnoldpayet@gmail.com>
2026-01-20 06:10:06 -0400

    many to many relationships

--- src/main/java/com/arnex/app/Main.java
+++ src/main/java/com/arnex/app/Main.java
@@ -5,8 +5,10 @@ import java.util.List;
 import com.arnex.app.entities.Author;
 import com.arnex.app.entities.Book;
 import com.arnex.app.entities.BookType;
+import com.arnex.app.entities.Group;
 import com.arnex.app.entities.Item;
 import com.arnex.app.entities.Review;
+import com.arnex.app.entities.User;
 import com.arnex.app.entities.keys.ItemKey;
 
 import jakarta.persistence.EntityManager;
@@ -25,7 +27,8 @@ public class Main {
         // useRefresh(emf);
         // createEntityWithComposePK(emf);
         // oneToOneRelationship(emf);
-        oneToManyRelationship(emf);
+        // oneToManyRelationship(emf);
+        manyToManyRelationship(emf);
     }
 
     private static void createInstance(EntityManagerFactory emf) {
@@ -202,6 +205,34 @@ public class Main {
             book.setReviews(List.of(review1, review2));
 
             em.persist(book);
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    private static void manyToManyRelationship(EntityManagerFactory emf) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+
+            User user1 = new User();
+            user1.setName("User1");
+            User user2 = new User();
+            user2.setName("User2");
+
+            Group group1 = new Group();
+            group1.setName("Group1");
+            Group group2 = new Group();
+            group2.setName("Group2");
+
+            group1.setUsers(List.of(user1, user2));
+            group2.setUsers(List.of(user1));
+
+            em.persist(group1);
+            em.persist(group2);
 
             em.getTransaction().commit();
         } finally {
---
+++ src/main/java/com/arnex/app/entities/User.java
@@ -0,0 +1,50 @@
+package com.arnex.app.entities;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @ManyToMany(mappedBy = "users")
+    private List<Group> groups;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+}
